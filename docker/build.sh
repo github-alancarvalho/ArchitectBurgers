817eb2ad3d0f34b5930d2b2178f38842284a64d2
Rodrigo Gomes <rodrigo.gomes@viasat.com>
2024-06-26 21:16:14 -0300

    new docker build using builder container. + increment image version

--- docker/build.sh
+++ docker/build.sh
@@ -3,20 +3,25 @@
 cd $(dirname $0) || exit 1
 
 cd ..
-./mvnw -DskipTests install dependency:copy-dependencies
 
-cd -
+# For now doing the build in a builder container. Review when integrating with CI/CD
+#
+#./mvnw -DskipTests install dependency:copy-dependencies
+#
+#cd -
+#
+##rm -rf temp_libs
+##mkdir temp_libs
+##cp ../web-api/target/architect-burgers-webapi-*.jar ./temp_libs/ || exit 1
+##cp ../web-api/target/lib/* ./temp_libs/ || exit 1
+#
+#if [ $? -ne 0 ]
+#then
+#	echo "Package failed"
+#	exit 1
+#fi
 
-rm -rf temp_libs
-mkdir temp_libs
-cp ../web-api/target/architect-burgers-webapi-*.jar ./temp_libs/ || exit 1
-cp ../web-api/target/lib/* ./temp_libs/ || exit 1
+docker build . -f docker/Dockerfile -t gomesrodris/architect-burgers:0.0.3-beta
 
-if [ $? -ne 0 ]
-then
-	echo "Package failed"
-	exit 1
-fi
-
-docker build . -t gomesrodris/architect-burgers:0.0.2
+docker.io/gomesrodris/architect-burgers:0.0.3-beta
 
